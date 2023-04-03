c1f206f6b54016c07e269934cbbac709fd910db2
root <karachidavid31@gmail.com>
2023-04-03 16:03:49 +0000

    check for errors

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
-import Router from 'express';
+import {
+  Router,
+} from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';

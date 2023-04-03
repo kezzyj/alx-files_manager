caf20982db57d6d6f3af07649096e616634948be
root <karachidavid31@gmail.com>
2023-04-03 14:00:13 +0000

    first api

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 import express from 'express';
-import AppController from './controllers/AppController';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/status', (req, res) => {
-  const status = {
-    redis: AppController.isRedisAlive(),
-    db: AppController.isDBAlive(),
-  };
-  res.status(200).send(status);
-});
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(router);
 
-app.get('/stats', async (req, res) => {
-  const nbUsers = await AppController.getNbUsers();
-  const nbFiles = await AppController.getNbFiles();
-  const stats = {
-    users: nbUsers,
-    files: nbFiles,
-  };
-  res.status(200).send(stats);
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
